314034b43d203286f03ed80a5b981f61afb8a45a
Hayden Chung <haydenchung1289@gmail.com>
2023-07-24 12:00:23 +1200

    Motivational Quotes (default quotes)
    NEW:
    - added default quotes to the quotes list

---
+++ assets/defaultQuotes.js
@@ -0,0 +1,13 @@
+export const defaultQuotes = [
+    ["All our dreams can come true, if we have the courage to pursue them", "Walt Disney", false],
+    ["The secret of getting ahead is getting started", "Mark Twain", false ],
+    ["Whatever you are, be a good one", "Abraham Lincoln", false],
+    ["The best time to plant a tree was 20 years ago. The second best time is now", "Chinese Proverb", false],
+    ["You’ve gotta dance like there’s nobody watching, love like you’ll never be hurt, sing like there’s nobody listening, and live like it’s heaven on earth", "William W. Purkey", false],
+    ["Do one thing every day that scares you", "Eleanor Roosevelt", false],
+    ["It’s no use going back to yesterday, because I was a different person then", "Lewis Carroll", false],
+    ["You can either experience the pain of discipline or the pain of regret. The choice is yours", "", false],
+    ["People who wonder if the glass is half empty or full miss the point. The glass is refillable", "", false],
+    ["If something is important enough, even if the odds are stacked against you, you should still do it", "Elon Musk", false]
+]; 
+
