047609a323838d7391f65c01d2f73ec46022ab17
Hayden Chung <haydenchung1289@gmail.com>
2023-08-14 11:54:47 +1200

    commited from laptop
    NEXT:
    - look at previous commit.
    - translate algorithm to JS

--- test2.py
+++ test2.py
@@ -1,17 +1,19 @@
-dict = {'task1': [2], 'task2': [1.5], 'task3': [0.5], 'task4': [1]}
-
+dict = {'task1': [3], 'task2': [6], 'task3': [9], 'task4': [4], 'task5': [12]}
+target = 13
 def find_closest_subset_sum(nums, target):
     dp = [False] * (target + 1)
     dp[0] = True
-    
+    print('before', dp)
+
     for num in nums:
         for i in range(target, num[0] - 1, -1): # 17, (12-1=)11, -1
             dp[i] = dp[i] or dp[i - num[0]] # if dp[i] or dp[i -num] is True, set to True. else, set to false
-        print(f'dp: {dp}, dp[i]: {dp[i]}, dp[i - num]: {dp[i-num[0]]}')
- 
+    print('after', dp)
+
+
+
     closest_sum = 0
     for i in range(target, -1, -1): # find the closest number to the target. 
-        print(i, 'h')
         if dp[i]:
             closest_sum = i
             break
@@ -19,10 +21,9 @@ def find_closest_subset_sum(nums, target):
     return closest_sum
 
 # Example usage
-nums = [6, 4, 1]
-target = 17
+
 values = dict.values()
-closest_sum = find_closest_subset_sum(nums, target)
+closest_sum = find_closest_subset_sum(values, target)
 print("Closest sum:", closest_sum)
 
 print(f'keys {values}')
