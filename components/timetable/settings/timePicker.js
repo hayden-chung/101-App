f3d20532ab927efbfe5d25579d70f66cdc4d8c85
Hayden Chung <haydenchung1289@gmail.com>
2023-08-19 00:52:51 +1200

    Timetable Generator (Finish time is always greater than starting time)
    NEW:
    - new feature to ensure the finishing time is always greater than the starting time for future algorithms to function properly.
    
    NEXT:
    - create algorithm to generate timetable

--- components/timetable/settings/timePicker.js
+++ components/timetable/settings/timePicker.js
@@ -7,22 +7,34 @@ const SCREEN_WIDTH = Dimensions.get('window').width;
 const SCREEN_HEIGHT = Dimensions.get('window').height;
 
 const TimePicker = ({sessionType, startOrFinish}) => { 
-    const [time, setTime] = useState(new Date());
+    const [time, setTime] = useState(new Date()); // initialize 'time' with current date and time. Gets the date and time of when the date picker was first opened. 
     const [show, setShow] = useState(false);
     const [text, setText] = useState(fixedSessions[sessionType][startOrFinish]);
-    console.log(text)
     
-    const onChange = (event, selectedTime) => { // When date on date picker changes. 
+    const onChange = (event, selectedTime) => { // event = cancel/ok button, selectedTime = only exists if user presses ok button.
+        if (event.type === 'set' && startOrFinish === 1) { // ok button pressed and setting finish time
+            if (selectedTime <= fixedSessions[sessionType][0]) {
+                const tempTime = new Date(fixedSessions[sessionType][0]);
+                tempTime.setMinutes(fixedSessions[sessionType][0].getMinutes() + 10);
+
+
+                console.log('Current Date:', selectedTime);
+                selectedTime = tempTime;
+                console.log('New Date with 10 minutes added:', selectedTime);
+            } 
+        }
         const currentTime = selectedTime || time; // If there is a selected time, else, initial date (new Date()). 
-        setShow(Platform.OS === 'ios'); // For iOS only. Boolean for if date picker shows or not. 
+        setShow(false); // date picker hides now
         setTime(currentTime); // Set time to currently selected time. 
 
-        let newTime = new Date(currentTime);
-        setText(newTime)
-        updateFixedSessions(sessionType, startOrFinish, newTime)
+        let newTime = new Date(currentTime); // create a new Date object to manipulate rather than changing the original 'currentTime' object. 
+        setText(newTime) // For displaying the time.
+        updateFixedSessions(sessionType, startOrFinish, newTime) // Update the time for this session. 
+        console.log('event', event, 'selected time', selectedTime, 'ti stil prints')
+
     }
 
-    const showMode = () => {
+    const showMode = () => { // Show time picker (true/false).
         setShow(true);
     }
 
@@ -33,12 +45,12 @@ const TimePicker = ({sessionType, startOrFinish}) => {
                 {text !== 'Empty' ?  <Text> {text.getHours()}:{text.getMinutes()} </Text> : <Text>Empty</Text>}
             </TouchableOpacity>
 
-            {show && (
+            {show && ( // if 'show' is true
                 <DateTimePicker
                 testID='dateTimerPicker'
                 value={time}
-                mode='time'
-                is24Hour={true}
+                mode='time' // timer picker (not date picker)
+                is24Hour={false}
                 display='default'
                 onChange={onChange}
                 />
--- components/timetable/timetableSettingsData.js
+++ components/timetable/timetableSettingsData.js
@@ -3,7 +3,6 @@ import {useState} from 'react';
 export const fixedSessions = {'start-finish':['Empty', 'Empty'], 'afternoon tea':['Empty', 'Empty'],'dinner':['Empty', 'Empty'], 'another break':['Empty', 'Empty']};
 
 export const updateFixedSessions = (key, index, value) => {
-    console.log('before', fixedSessions)
     fixedSessions[key][index] = value;
     console.log('after', fixedSessions)
 }
