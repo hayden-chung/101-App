41feed192bafa4a880f072fdb1aff1a887a3cf6e
Hayden Chung <haydenchung1289@gmail.com>
2023-08-21 18:25:50 +1200

    Merge branch 'main' of https://github.com/hayden-chung/101-App

--- app/timetableScreen.js
+++ app/timetableScreen.js
@@ -1,9 +1,17 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import { KeyboardAvoidingView, StyleSheet, Text, View, FlatList, TouchableOpacity} from 'react-native';
 import {timetable} from '../components/timetable/timetableGenerator'
-import {TimetableGenerator} from '../components/timetable/timetableGenerator'
 import TimeBlock from '../components/timetable/timetableBlocks'
 
+// const setToTimeObject = () => {
+//     for (let i = 0; i < timetable.length; i++) {
+//         if (timetable[3]) {
+//             console.
+//         }
+//     }
+// } 
+
+const TimetableScreen = ({navigation}) => { 
 // const setToTimeObject = () => {
 //     for (let i = 0; i < timetable.length; i++) {
 //         if (timetable[3]) {
@@ -16,20 +24,10 @@ import TimeBlock from '../components/timetable/timetableBlocks'
 const TimetableScreen = ({navigation}) => { 
 
     console.log('====================================================================================================================================')
-    console.log('testing in screen', timetableData)  
-
-    // useEffect(() => {
-    //     console.log('TimetableData has changed:', timetableData);
-    // }, [timetableData]);
-
-    const handleGenerate = (updatedTimetableData) => {
-        const copyUpdatedTimetableData = [...updatedTimetableData]
-        setTimetableData(copyUpdatedTimetableData);
-        console.log('TIMETABLE DATA', timetableData, 'copied', copyUpdatedTimetableData)
-    };
-
-    return( 
+    console.log('testing in screen', timetable)  
+    return(
         <View>
+            <TouchableOpacity onPress={() => navigation.navigate("TimetableGenerator")}>
 
             {/* Generate New Timetable Button. When pressed, navigate to 'TimetableGenerator' screen. */}
             <TouchableOpacity onPress={() => 
@@ -43,6 +41,7 @@ const TimetableScreen = ({navigation}) => {
                  
                 <FlatList
                     data = {timetable}
+                    data = {timetable}
                     renderItem={({item, index}) => 
                     <TimeBlock
                         item={item}
@@ -50,6 +49,8 @@ const TimetableScreen = ({navigation}) => {
                     />
 
                 }/>
+
+            
 
             <TouchableOpacity onPress={() => navigation.navigate("TimetableGenerator")}>
                 <Text>New Timetable</Text>
